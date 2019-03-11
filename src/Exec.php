158b4ba1ad974388d0d6f3cf668c3b8eca660268
xltxlm <xltxlm@qq.com>
2019-03-11 11:23:55 +0800

    执行结果,可以存在到redis里面观察

--- src/Exec.php
+++ src/Exec.php
@@ -2,7 +2,9 @@
 
 namespace xltxlm\shell;
 
+use xltxlm\logger\thelostlog_exec\thelostlog_exec;
 use xltxlm\shell\Exception\Exception_Exec;
+use xltxlm\shell\Exec\Exec_implements;
 
 
 /**
@@ -21,18 +23,54 @@ class Exec extends Exec\Exec_implements
         }
     }
 
+    /**
+     * @param \Redis $RedisObject ;
+     * @return $this
+     */
+    public static function setRedisObject(\Redis $RedisObject)
+    {
+        Exec::$RedisObject = $RedisObject;
+        if (is_object(Exec::$RedisObject) && $_SERVER['logid']) {
+            Exec::$RedisObject->zAdd($_SERVER['logid'], Exec::getcounti(),'Begin');
+            //保存10分钟
+            Exec::$RedisObject->expire($_SERVER['logid'], 60 * 10);
+        }
+    }
+
+    /**
+     * @return int;
+     */
+    public static function getcounti(): int
+    {
+        Exec::$counti++;
+        return Exec::$counti;
+    }
+
+
     public function __invoke(): string
     {
+        //如果声明了Redis存储,那么每一个命令都会给存储,保存10分钟
+        if (is_object(Exec::$RedisObject) && $_SERVER['logid']) {
+            Exec::$RedisObject->zAdd($_SERVER['logid'], Exec::getcounti(),$this->getCmd());
+        }
+        $thelostlog_exec = (new thelostlog_exec())
+            ->setcommand($this->getCmd());
         $start = microtime(true);
         exec($this->getCmd(), $out, $return);
         $time = sprintf('%.4f', microtime(true) - $start);
+        $ressult = join("\n", $out);
         if ($this->isDebug()) {
-            p(["cmd" => $this->getCmd(), "time" => $time, 'return' => $return]);
+            p(["cmd" => $this->getCmd(), "time" => $time, 'ressult' => $ressult, 'return' => $return]);
         }
+        if (is_object(Exec::$RedisObject) && $_SERVER['logid']) {
+            Exec::$RedisObject->zAdd($_SERVER['logid'], Exec::getcounti(),"return code:$return");
+        }
+        $thelostlog_exec->setresult($ressult);
         if ($this->isException_on_Fail() && $return) {
+            $thelostlog_exec->seterror($return);
             throw new Exception_Exec("执行命令[{$this->getCmd()}]错误:" . join($out) . ",错误值:$return");
         }
-        return join("\n", $out);
+        return $ressult;
     }
 
 
--- src/Exec/Exec_implements.php
+++ src/Exec/Exec_implements.php
@@ -2,104 +2,175 @@
 namespace xltxlm\shell\Exec;
 
 /**
+ * :类;
  * 执行系统命令,错误会抛出异常;
 */
 abstract class Exec_implements
 {
 
 
-
-    /* @var string  要执行的命令 */
-        protected $Cmd = '';
-    
-
-
-    /**
-     * @return string;
-     */
-    public function getCmd():string    {
-        return $this->Cmd;
-    }
-
+/* @var string  要执行的命令 */
+    protected $Cmd = '';
 
 
 
 
 
     /**
-     * @param string $Cmd;
-     * @return $this
-     */
-    public function setCmd(string $Cmd)
-    {
-        $this->Cmd = $Cmd;
-        return $this;
-    }
-
-    /* @var bool  是否在php错误文件输出执行的命令 */
-        protected $Debug = true;
-    
-
-
-    /**
-     * @return bool;
-     */
-    public function getDebug():bool    {
-        return $this->Debug;
-    }
-
-
-    public function isDebug():bool    {
-            return $this->getDebug();
-    }
-    
-
-
-
-    /**
-     * @param bool $Debug;
-     * @return $this
-     */
-    public function setDebug(bool $Debug)
-    {
-        $this->Debug = $Debug;
-        return $this;
-    }
-
-    /* @var bool  错误的时候抛出异常信息 */
-        protected $Exception_on_Fail = true;
-    
-
-
-    /**
-     * @return bool;
-     */
-    public function getException_on_Fail():bool    {
-        return $this->Exception_on_Fail;
-    }
-
-
-    public function isException_on_Fail():bool    {
-            return $this->getException_on_Fail();
-    }
-    
-
-
-
-    /**
-     * @param bool $Exception_on_Fail;
-     * @return $this
-     */
-    public function setException_on_Fail(bool $Exception_on_Fail)
-    {
-        $this->Exception_on_Fail = $Exception_on_Fail;
-        return $this;
-    }
-
-    /**
-     *   返回命令的内容;
-     *   @return :string;
+    * @return string;
     */
-    abstract public function __invoke():string;
+            public function getCmd():string        {
+                return $this->Cmd;
+        }
 
+    
+    
+
+
+
+
+/**
+* @param string $Cmd;
+* @return $this
+*/
+    public function setCmd(string $Cmd  = "")
+    {
+    $this->Cmd = $Cmd;
+    return $this;
+    }
+
+
+
+/* @var bool  是否在php错误文件输出执行的命令 */
+    protected $Debug = true;
+    
+
+
+
+
+    /**
+    * @return bool;
+    */
+            public function getDebug():bool        {
+                return $this->Debug;
+        }
+
+    
+            public function isDebug():bool        {
+        return $this->getDebug();
+        }
+    
+
+
+
+
+/**
+* @param bool $Debug;
+* @return $this
+*/
+    public function setDebug(bool $Debug  = true)
+    {
+    $this->Debug = $Debug;
+    return $this;
+    }
+
+
+
+/* @var bool  错误的时候抛出异常信息 */
+    protected $Exception_on_Fail = true;
+    
+
+
+
+
+    /**
+    * @return bool;
+    */
+            public function getException_on_Fail():bool        {
+                return $this->Exception_on_Fail;
+        }
+
+    
+            public function isException_on_Fail():bool        {
+        return $this->getException_on_Fail();
+        }
+    
+
+
+
+
+/**
+* @param bool $Exception_on_Fail;
+* @return $this
+*/
+    public function setException_on_Fail(bool $Exception_on_Fail  = true)
+    {
+    $this->Exception_on_Fail = $Exception_on_Fail;
+    return $this;
+    }
+
+
+
+/* @var \Redis  存储全部执行过程命令的Redis服务器 */
+    public  static $RedisObject;
+
+
+
+
+
+    /**
+    * @return \Redis;
+    */
+            public static  function getRedisObject():\Redis        {
+            return static::$RedisObject;
+        }
+
+    
+    
+
+
+
+
+/**
+* @param \Redis $RedisObject;
+* @return $this
+*/
+    abstract public static  function setRedisObject(\Redis $RedisObject );
+
+
+
+/* @var int  记录的顺序 */
+    public  static $counti = 0;
+    
+
+
+
+
+    /**
+    * @return int;
+    */
+            abstract public static  function getcounti():int;
+    
+    
+
+
+
+
+/**
+* @param int $counti;
+* @return $this
+*/
+    public static  function setcounti(int $counti  = 0)
+    {
+    static::$counti = $counti;
+    return $this;
+    }
+
+
+
+/**
+*  返回命令的内容;
+*  @return :string;
+*/
+abstract public function __invoke():string;
 }
